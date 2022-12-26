41b7f38d8811433177d8342e4b34bcdd89905815
0xpaluco <hello@0xpalu.co>
2022-12-25 23:45:52 -0400

    mobile account nav section

--- src/components/dashboard/dashboard-nav.tsx
+++ src/components/dashboard/dashboard-nav.tsx
@@ -113,21 +113,8 @@ export default function DashboardNavigation({ children }: DashboardNavProps) {
                                     </nav>
                                 </div>
                                 <div className="flex flex-shrink-0 bg-gray-700 p-4">
-                                    <Link href="/account" className="group block flex-shrink-0">
-                                        <div className="flex items-center">
-                                            <div>
-                                                <img
-                                                    className="inline-block h-10 w-10 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                    alt=""
-                                                />
-                                            </div>
-                                            <div className="ml-3">
-                                                <p className="text-base font-medium text-white">Tom Cook</p>
-                                                <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">View Account</p>
-                                            </div>
-                                        </div>
-                                    </Link>
+                                    <AcccountNav />
+                                   
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
