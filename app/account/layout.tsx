47375075178af8319cb589aa26bd81feb6f40a5d
0xpaluco <hello@0xpalu.co>
2023-01-22 23:56:10 -0400

    color changes

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -11,13 +11,13 @@ export const revalidate = 0
 export default async function DashboardLayout({ children }: LayoutProps) {
 
     return (
-        <html lang="en" className='bg-gray-900 h-full'>
+        <html lang="en" className='bg-gray-200 h-full'>
             <head>
                 {/* Used to be added by default, now we need to add manually */}
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width" />
             </head>
-            <body className='bg-gray-900 h-full'>
+            <body className='bg-gray-200 h-full'>
                 <DashboardNav>
                     {children}
                 </DashboardNav>
