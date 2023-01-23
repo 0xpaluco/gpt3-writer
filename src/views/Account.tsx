43f504e8306d97f13f24cf5939d7ae737da92439
0xpaluco <hello@0xpalu.co>
2023-01-23 00:02:28 -0400

    color changes

--- src/views/Account.tsx
+++ src/views/Account.tsx
@@ -84,42 +84,44 @@ export default function Account({ session }: { session?: Session }) {
 
   return (
     <>
-      <div className='p-4 sm:p-12'>
+      <div className='p-0 sm:p-12'>
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-300">Account</h3>
-              <p className="mt-1 text-sm text-gray-400">
+              <h3 className="text-lg font-medium leading-6 text-gray-800">Account</h3>
+              <p className="mt-1 text-sm text-gray-700">
                 Basic account information.
               </p>
             </div>
           </div>
           <div className="mt-5 md:col-span-2 md:mt-0">
             <form action="#" method="POST">
-              <div className="shadow sm:overflow-hidden sm:rounded-md">
-                <div className="space-y-6 bg-gray-700 px-4 py-5 sm:p-6">
+              <div className="shadow sm:overflow-hidden sm:rounded-md bg-gray-200">
+                <div className="space-y-6 px-4 py-5 sm:p-6">
                   <div className="grid grid-cols-3 gap-6">
                     <div className="col-span-3 sm:col-span-2">
-                      <label htmlFor="company-website" className="block text-sm font-medium text-gray-300">
+                      <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
                         Website
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-500 px-3 text-sm text-gray-300">
+                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-100 px-3 text-sm text-gray-700">
                           http://
                         </span>
                         <input
                           type="text"
                           name="company-website"
                           id="company-website"
-                          className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-200 bg-gray-600"
+                          className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-700 bg-gray-100"
                           placeholder="www.example.com"
+                          value={website || ''}
+                          onChange={(e) => setWebsite(e.target.value)}
                         />
                       </div>
                     </div>
                   </div>
 
                   <div>
-                    <label htmlFor="full_name" className="block text-sm font-medium text-gray-300">
+                    <label htmlFor="full_name" className="block text-sm font-medium text-gray-700">
                       Full Name
                     </label>
                     <div className="mt-1">
@@ -127,14 +129,14 @@ export default function Account({ session }: { session?: Session }) {
                         id="full_name"
                         name="full_name"
                         type="text"
-                        className="block w-full flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-200 bg-gray-600"
+                        className="block w-full flex-1 rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-700 bg-gray-100"
                         placeholder="John Smith"
                         defaultValue={''}
                         value={full_name || ''}
                         onChange={(e) => setFullName(e.target.value)}
                       />
                     </div>
-                    <p className="mt-2 text-sm text-gray-300">
+                    <p className="mt-2 text-sm text-gray-500">
                       Brief description for your profile. URLs are hyperlinked.
                     </p>
                   </div>
@@ -154,7 +156,7 @@ export default function Account({ session }: { session?: Session }) {
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <div>
                     <button
-                      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-slate-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
                       onClick={() => updateProfile({ full_name, website, avatar_url })}
                       disabled={loading}
                     >
