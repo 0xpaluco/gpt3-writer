c86a64253a3ca6aa1e44ab431305596ee439294a
0xpaluco <hello@0xpalu.co>
2023-01-25 00:18:28 -0400

    margin

--- src/components/auth/signout-button.tsx
+++ src/components/auth/signout-button.tsx
@@ -19,7 +19,7 @@ export default function SignOutButton() {
           </div>
         </div>
 
-        <div className="bg-white shadow sm:rounded-lg">
+        <div className="bg-white shadow sm:rounded-lg mt-5 md:col-span-2 md:mt-0">
           <div className="px-4 py-5 sm:p-6">
             <h3 className="text-lg font-medium leading-6 text-gray-900">Sign out of your account</h3>
             <div className="mt-2 max-w-xl text-sm text-gray-500">
